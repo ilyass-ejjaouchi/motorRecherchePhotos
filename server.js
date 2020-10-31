8dfddc20967947c97378ef290445f632bd344797
ilyass <ilyass.ezzaouchi@gmail.com>
2020-10-31 19:58:49 +0100

    heroku

--- server.js
+++ server.js
@@ -1,59 +1,12 @@
-const express = require('express');
 const path = require('path');
-const axios = require('axios');
-
-// Loads env variables
-require('dotenv').config()
-
+const express = require('express');
 const app = express();
-
-const PORT = process.env.PORT || 3001;
-
-// Adds json parsing middleware
-app.use(express.json());
-
-// Setup static directory to serve
-app.use(express.static(path.resolve('client', 'build')));
-
-// Creates weather endpoint
-app.post('/weather', async (req, res) => {
-    const { location } = req.body
-
-    // Encode the variable so we can send the location in a URL
-    const encodedLocation = encodeURIComponent(location)
-
-    try {
-        // Call the Weather API
-        const { data } = await axios({
-            method: "GET",
-            url: `https://aerisweather1.p.rapidapi.com/observations/${encodedLocation}`,
-            headers: {
-                "content-type": "application/octet-stream",
-                "x-rapidapi-host": "aerisweather1.p.rapidapi.com",
-                "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-                "useQueryString": true
-            }
-        })
-
-        // Pull the information that we need from the Weather API response
-        const weatherData = {
-            conditions: data.response.ob.weather,
-            tempC: data.response.ob.tempC,
-            tempF: data.response.ob.tempF
-        }
-
-        // Return the data object
-        return res.send(weatherData)
-    } catch (e) {
-        console.log(e)
-
-        return res.status(500).send('Error.')
-    }
-})
-
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-// console.log that your server is up and running
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(port, () => {
+   console.log('Server is up!');
+});
