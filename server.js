a8aefe10f1e7b77b0d57cf072b275258e9e1ea3e
ilyass <ilyass.ezzaouchi@gmail.com>
2020-10-31 20:02:54 +0100

    heroku 1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(publicPath, 'index.js'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
